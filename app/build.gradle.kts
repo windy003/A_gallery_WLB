74410570219e9062dfea0b6407f89500238a6346
windy003 <928920471@qq.com>
2025-07-09 19:28:00 +0800

    添加了版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 33
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "2025/7/9-01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
